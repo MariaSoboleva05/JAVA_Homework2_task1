c35caa7e83e8b15acad8bd05c35d085a1593e966
Maria Soboleva <mmouse.mv03@gmail.com>
2022-06-16 22:59:45 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ticketCost = 10_000;
+        int mileCost = 20;
+        int mile = ticketCost / mileCost;
+
+        System.out.println("Количество начисленных миль - " + mile);
+
+
+    }
+}
+
